1a314bcd94611357f205c1ce993697cfc4d0919a
CleitonBrito <cleytonbritto3003@gmail.com>
2024-09-17 23:56:12 -0300

    create card home items

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,4 +1,5 @@
 import Layout from "../../Components/Layout"
+import Card from '../../Components/Card'
 import { IoLocation  } from "react-icons/io5";
 import pizzaImg from '../../assets/PizzaFlat.png'
 import salgadosImg from '../../assets/Salgados.png'
@@ -9,7 +10,7 @@ import './index.css'
 export default function Home(){
     return (
         <Layout>
-            <div className="flex flex-col sm:gap-y-1 gap-y-4 sm:w-7/12 mx-auto">
+            <div className="flex flex-col sm:gap-y-1 gap-y-4 sm:w-7/12 mx-auto h-screen">
                 <section className="leading-3 flex justify-center h-18 items-center flex-col">
                     <div className="flex gap-2 items-center">
                         <IoLocation size={18} color="red"/>
@@ -19,7 +20,7 @@ export default function Home(){
                         Praça das Assembléias, Bairro Novo, Filadélfia - BA
                     </span>
                 </section>
-                <section className="mx-auto w-full grid grid-rows-none grid-cols-4 h-20 items-center justify-items-center leading-4 bg-white rounded-md">
+                <section className="mx-auto w-full grid grid-rows-none grid-cols-4 h-[12rem] items-center justify-items-center leading-4 bg-white rounded-md">
                     <div className="flex flex-col items-center">
                         <div className="size-14 rounded-full products-img flex justify-center items-center">
                             <img className="size-12 rounded-full products-img object-scale-down" src={ pizzaImg } alt="" />
@@ -45,8 +46,25 @@ export default function Home(){
                         <span className="text-sm">Bebidas</span>
                     </div>
                 </section>
-                <section className="main bg-white rounded-md h-screen">
-                    
+                <section className="py-2 px-4 pb-52 bg-white rounded-md overflow-y-scroll">
+                    <div className="pb-10">
+                        <div className="flex justify-between p-2 pb-0">
+                            <h1 className="font-bold text-xl">Pizzas</h1>
+                            <span className="font-bold text-[#bbab31]">A partir de R$33,00</span>
+                        </div>
+                        <Card title="Pizza de Calabresa" description="Calabresa, Tomate, Queijo e Orégano" image={pizzaImg} />
+                        <Card title="Pizza de Carne Seca" description="Carne Seca, Tomate, Queijo, Azeitonas e Orégano" image={pizzaImg} />
+                        <Card title="Pizza Mexicana" description="Calabresa, Bacon, Muçarela, Cebola e Orégano" image={pizzaImg} />
+                    </div>
+                    <div>
+                        <div className="flex justify-between">
+                            <h1 className="font-bold text-xl">Salgados</h1>
+                            <span className="font-bold text-[#bbab31]">A partir de R$4,00</span>
+                        </div>
+                        <Card title="Pastel de Frango" description="Frango, Queijo e Catupiry" image={salgadosImg} />
+                        <Card title="Pastel de Presunto" description="Presunto e Queijo" image={salgadosImg} />
+                        <Card title="Pastel de Calabresa" description="Calabresa e Queijo" image={salgadosImg} />
+                    </div>
                 </section>
             </div>
         </Layout>
