5a0a5273211f0c6426fca37df73d93ff83457bc4
CleitonBrito <cleytonbritto3003@gmail.com>
2024-09-19 22:52:37 -0300

    links to footer bar

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,11 +1,20 @@
-import React, { Component } from "react"
+import React from "react"
+import { TbArrowBackUp } from "react-icons/tb";
+import { TiHome } from "react-icons/ti";
+import { useNavigate } from 'react-router-dom';
 
-class Footer extends Component {
-    render(){
-       return <div className="px-5 sticky bottom-0 h-12 w-screen sm:hidden block mx-auto bg-themeMainDefault text-white">
-            Footer Bar
+export const Footer = () => {
+    let navigate = useNavigate();
+    return <div className="px-5 sticky bottom-0 h-12 w-screen sm:hidden flex justify-center items-center mx-auto bg-themeMainDefault text-white">
+        <div className="flex w-6/12 justify-evenly">
+            <button onClick={() => navigate(-1) } >
+                <TbArrowBackUp size={30} color="#1E1B4B" className="bg-white rounded-md"/>
+            </button>
+            <button disabled={ window.location.pathname === "/" ? "disabled" : "" } onClick={() => navigate("/")}>
+                <TiHome size={30} color="#1E1B4B" className="p-1 bg-white rounded-md"/>
+            </button>
         </div>
-    }
+    </div>
 }
 
 export default Footer;
