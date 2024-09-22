664a84e734b10769e542c38b0403c707c53f9a05
CleitonBrito <cleytonbritto3003@gmail.com>
2024-09-22 18:17:43 -0300

    update animate velocity

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,12 @@ module.exports = {
             themeMainDefault : "#1E1B4B"
         },
         animation: {
-            'opacity' : 'opacity 0.8s ease-in-out',
+            'opacity' : 'opacity 1s ease-in-out',
         },
         keyframes: {
             'opacity': {
                 '0%' : {opacity: 0},
-                '70%' : {opacity: 95},
+                '70%' : {opacity: 90},
                 '100%' : {opacity: 100}
             }
         }
