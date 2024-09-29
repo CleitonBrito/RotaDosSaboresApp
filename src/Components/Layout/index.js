596cfa8d523f774e0a75dd4a7656bea82b99c42e
CleitonBrito <cleytonbritto3003@gmail.com>
2024-09-29 12:59:05 -0300

    feature: make cart floating

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,13 +1,21 @@
 import Header from "../../Components/Header"
 import Footer from "../../Components/Footer"
 import './index.css'
+import ResumeCart from "../ResumeCart"
 
 const Layout = ({ children }) => {
+    const location = window.location.pathname;
     return (
         <div className="layout bg-gradient-to-b to-[#ABABAB] from-[#FFFFFF] h-screen grid grid-rows-[5rem_auto]">
             <Header />
             <main className="flex flex-col h-screen w-screen items-center px-4 py-2">
-                <div className="animate-opacity w-full md:w-7/12 lg:w-6/12 xl:w-4/12">{ children }</div>
+                <div className="animate-opacity w-full md:w-7/12 lg:w-6/12 xl:w-4/12">
+                    { children }
+                </div>
+                {
+                location === '/cart' ? '' :
+                <ResumeCart />
+                }
             </main>
             <Footer />
         </div>
---
+++ src/Components/ResumeCart/index.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import { Link } from "react-router-dom"
+import './index.css'
+
+class ResumeCart extends Component{
+    render(){
+    return <section className="overlay absolute bottom-0 flex flex-col justify-center -translate-y-8 min-h-32 w-full md:w-7/12 lg:w-6/12 xl:w-4/12 bg-gradient-to-t from-[#000]/80 backdrop-blur-[2px]">
+        <section className="flex mt-8">
+            <Link to="/cart" className="relative grid grid-cols-[2fr_3fr] px-7 min-h-16 justify-center items-center w-11/12 mx-auto sm:mr-5 bg-gradient-to-r from-[#480097] to-[#280266] hover:brightness-110 hover:cursor-pointer text-white rounded-xl">
+                <div className="flex flex-col gap-0">
+                    <span className="text-sm">Ver carrinho</span>
+                    <h1 className="text-[#00FF25] text-base font-extrabold">5 Itens</h1>
+                </div>
+                <div className="flex items-center gap-2 justify-end">
+                    <h1 className="font-bold text-[#00FF25]">Subtotal</h1>
+                    <span className="bg-white px-3 py-1 rounded-full">
+                        <span className="font-extrabold bg-gradient-to-r from-[#0E7521] to-[#10DD10] text-transparent bg-clip-text sm:text-xl text-sm">R$ 33,00</span>
+                    </span>
+                </div>
+            </Link>
+        </section>
+    </section>
+    }
+}
+
+export default ResumeCart;
